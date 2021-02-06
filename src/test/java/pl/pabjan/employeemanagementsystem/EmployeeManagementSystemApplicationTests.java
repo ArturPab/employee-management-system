e3b9b83ddc94ddbb970f4942899a5c20521af126
Artur Pabjan <artur.pabjan00@gmail.com>
2021-02-06 15:10:33 +0100

    created new empty Employee Management System

---
+++ src/test/java/pl/pabjan/employeemanagementsystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pabjan.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
